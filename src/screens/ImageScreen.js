dc280c124714ee21b595c3cebf93bc65274158ca
roldevin <devinrolfe@gmail.com>
2019-04-27 17:03:36 -0400

    Added loading icon while fetching image data.

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {Text, View, TouchableOpacity} from "react-native";
+import {Text, View, TouchableOpacity, StyleSheet} from "react-native";
 import { Camera, Permissions } from 'expo';
 import { Entypo } from '@expo/vector-icons';
 import { Buffer } from 'buffer';
+import Loader from "../utils/Loader";
 var AWS = require('aws-sdk');
 
 export default class ImageScreen extends React.Component {
@@ -13,6 +14,7 @@ export default class ImageScreen extends React.Component {
             hasCameraPermission: null,
             type: Camera.Constants.Type.back,
             camera: null,
+            isLoading: false,
         };
 
         this.snap = this.snap.bind(this);
@@ -36,31 +38,60 @@ export default class ImageScreen extends React.Component {
     async snap() {
         if (this.camera) {
 
-            let photo = await this.camera.takePictureAsync({ quality: 0.5, base64: true });
+            this.camera.takePictureAsync({ quality: 0.5, base64: true })
+            .then(photo => {
+                let buffer = new Buffer(photo.base64, 'base64');
 
-            let buffer = new Buffer(photo.base64, 'base64');
+                let params = {
+                    Image: {
+                        Bytes: buffer
+                    }
+                };
 
-            let params = {
-              Image: {
-                  Bytes: buffer
-              }
-            };
+                this.rekognitionClient.detectText(params, function(err, data) {
+                    if (err) {
+                        console.log("Bad Call");
+                        console.log(err);
+                    } else {
+                        console.log("Good Call");
+                        console.log(data);
+                    }
+                });
 
-            this.rekognitionClient.detectText(params, function(err, data) {
-                if (err) {
-                    console.log("Bad Call");
-                    console.log(err);
-                } else {
-                    console.log("Good Call");
-                    console.log(data);
-                }
-            });
+                this.setState({
+                    isLoading: false,
+                })
 
+                // TODO:
+                // 1. Need to change screen to the picture that was taken
+                // 2. Add button on screen to go back to picture taking
+                // 3. Need to parse response from detectText
+                // 4. Need to highlight the text in the saved picture
+                // 5. Need to get definitions of each word (only supporting english)
+
+
+            }); // want to do catch here for error handling
+
+            // should do loading
+            this.setState({
+                isLoading: true,
+            })
 
         }
     };
 
     render() {
+
+        // Loading
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loadingContainer}>
+                    <Loader />
+                </View>
+            );
+        }
+
+
         const { hasCameraPermission } = this.state;
 
         if (hasCameraPermission === null) {
@@ -90,5 +121,13 @@ export default class ImageScreen extends React.Component {
         }
     };
 };
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+});
 
 
