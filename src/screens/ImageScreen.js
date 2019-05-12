8045f2bb8a17eae5eeef0cad4f7c79f39f3d60fa
roldevin <devinrolfe@gmail.com>
2019-05-12 13:14:46 -0400

    Error handling for camera unhandled promises.

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, TouchableOpacity, Image, Button, StyleSheet} from "react-native";
+import { Alert, Text, View, TouchableOpacity, Image, Button, ImageBackground, StyleSheet} from "react-native";
 import { Camera, Permissions } from 'expo';
 import { Entypo } from '@expo/vector-icons';
 import { Buffer } from 'buffer';
@@ -96,20 +96,28 @@ export default class ImageScreen extends React.Component {
                 });
 
                 // TODO:
-                // 1. Need to change screen to the picture that was taken (Done)
-                // 2. Add button on screen to go back to picture taking
-                // 3. Need to parse response from detectText
-                // 4. Need to highlight the text in the saved picture
-                // 5. Need to get definitions of each word (only supporting english)
+                // 1. Fix "Unhandled promise rejection: Error: Camera view had been unmounted before image has been captured" (Done)
+                // 2. Need to parse response from detectText
+                // 3. Need to highlight the text in the saved picture
+                // 4. Need to get definitions of each word (only supporting english)
+                // 5. hide accessKeys
 
+            })
+            .catch(() => {
+                Alert.alert("Oops, try again!");
 
-            }); // want to do catch here for error handling
+                this.setState({
+                    isLoading: false,
+                    cameraReadyPosition: true,
+                    imageTaken: null,
+                });
+            });
 
             // should do loading
             this.setState({
                 isLoading: true,
             })
-            
+
         }
     };
 
@@ -128,13 +136,19 @@ export default class ImageScreen extends React.Component {
         // if picture taken
         if (this.state.imageTaken && !this.state.cameraReadyPosition) {
             return (
-                <View style={{flex: 1,}}>
-                    <Image
-                        style={{flex:1, height: undefined, width: undefined}}
-                        resizeMode="contain"
-                        source={{uri: this.state.imageTaken}}
-                    />
-                </View>
+                <ImageBackground source={{uri: this.state.imageTaken}} style={{width: '100%', height: '100%'}}>
+                    <View style={{flex: 9,}}></View>
+                    <TouchableOpacity
+                        style={{
+                            alignItems:'center',
+                            flex: 1,
+                        }}
+                        onPress={this.resetCamera}
+                    >
+                        <Entypo name="ccw" size={64} color="white" />
+                    </TouchableOpacity>
+                    <View style={{flex: 0.2,}}></View>
+                </ImageBackground>
             )
         }
 
@@ -161,6 +175,7 @@ export default class ImageScreen extends React.Component {
                         >
                             <Entypo name="circle" size={64} color="white" />
                         </TouchableOpacity>
+                        <View style={{flex: 0.2,}}></View>
                     </Camera>
                 </View>
             )
@@ -174,6 +189,34 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center"
     },
+    resetScreenButton: {
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:10,
+        paddingBottom:10,
+        backgroundColor:'#000',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#fff'
+    },
+    resetText: {
+        color:'#fff',
+        textAlign:'center',
+        paddingLeft : 10,
+        paddingRight : 10
+    },
+
 });
+
+
+/*
+
+[Unhandled promise rejection: Error: Camera view had been unmounted before image has been captured]
+- node_modules/react-native/Libraries/BatchedBridge/NativeModules.js:146:41 in createErrorFromErrorData
+- node_modules/react-native/Libraries/BatchedBridge/NativeModules.js:95:55 in <unknown>
+- ... 5 more stack frames from framework internals
+
+ */
 
 
